381beeb44544b3fdf85f5a8d192e713b5fc70009
Hasan MEN <hmen.56@gmail.com>
2018-02-07 14:26:51 +0300

    Applying node2vec algorithm on dynamic words completed.

--- doc2graph.py
+++ doc2graph.py
@@ -1,19 +1,19 @@
 
 # coding: utf-8
 
-# In[100]:
+# In[1]:
 
 
 text = """Roberto Baggio (born 18 February 1967) is a retired Italian footballer. He is widely regarded as one of the finest footballers of all time (4th at a Fifa internet poll; member of the Fifa World Cup Dream Team). Baggio won both the Ballon d’Or and the FIFA World Player of the Year in 1993. He is the only Italian player ever scoring in three World Cups. He is also one of the top 5 all-time goalscorers for Italy. Baggio is known as Il Divin Codino (The Divine Ponytail), for the hairstyle he wore for most of his career and his Buddhist background. """
 
 
-# In[101]:
+# In[2]:
 
 
 text = text.lower()
 
 
-# In[102]:
+# In[3]:
 
 
 from nltk import tokenize
@@ -21,7 +21,7 @@ sentences = tokenize.sent_tokenize(text)
 print(sentences[:5])
 
 
-# In[103]:
+# In[4]:
 
 
 import re
@@ -29,67 +29,89 @@ pureSentences = [re.sub(r'[^\w\s]','',x) for x in sentences]
 print(pureSentences[:5])
 
 
-# In[104]:
+# In[5]:
 
 
 pureText = " ".join(pureSentences)
 print(pureText)
 
 
-# In[105]:
+# In[6]:
 
 
 splitText  = pureText.split(" ")
 print("SplittedPureText:",splitText)
 
 
-# In[106]:
+# In[7]:
 
 
 import networkx as nx
 graph = nx.Graph()
 
 
-# In[107]:
+# In[8]:
 
 
 graph.add_node(splitText[0])
 for i in range(1,len(splitText)):
     graph.add_node(splitText[i])
-    graph.add_edge(splitText[i-1],splitText[i])
+    graph.add_edge(splitText[i-1],splitText[i],weight=1)
 
 
-# In[108]:
+# In[9]:
 
 
 print("NumberOfNodes:",graph.number_of_nodes())
 print("NumberOfEdges:",graph.number_of_edges())
 
 
-# In[109]:
+# In[10]:
 
 
 print("Nodes:",graph.nodes())
 print("Edges:",graph.edges())
 
 
-# In[110]:
+# In[13]:
 
 
-import matplotlib.pyplot as plt
+nx.draw_circular(graph,node_size=60,font_size=6,with_labels=True)
 
 
-# In[122]:
+# In[14]:
 
 
-nx.draw(graph,pos=nx.spring_layout(graph),node_size=60,font_size=6)
-nx.draw_networkx_labels(graph,pos=nx.spring_layout(graph))
+import matplotlib.pyplot as plt 
+plt.savefig("a.pdf") # save graph to pdf file
 
 
-# In[124]:
+# In[16]:
 
 
-plt.figure(3,figsize=(12,12)) 
-plt.savefig("test.png",format="PNG")
-plt.show()
+from node2vec import Node2Vec
+
+
+# In[17]:
+
+
+node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4) 
+
+
+# In[18]:
+
+
+model = node2vec.fit(window=10, min_count=1, batch_words=4)  # Any keywords acceptable by gensim.Word2Vec can be passed, `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
+
+
+# In[21]:
+
+
+model.wv.most_similar("roberto")  # Output node names are always strings
+
+
+# In[20]:
+
+
+model.wv.save_word2vec_format("save.txt")
 
