85130866eda36d185d460201ccba34b3be1fdf60
Hasan MEN <hmen.56@gmail.com>
2018-02-10 00:12:52 +0300

    repo organization and readme updates
    project updated to saving outputs in a good format

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-doc2graph
+Run:
+* pip install -r requirements
+* python3 doc2graph.py
+
+Articles:  
+* [node2vec: Scalable Feature Learning for Networks](https://cs.stanford.edu/people/jure/pubs/node2vec-kdd16.pdf)
+* [A graph model for text analysis and text mining](#)
+* [Graph-theoretic techniques for web content mining](#) - Chapter 4
--- doc2graph.py
+++ doc2graph.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[15]:
+# In[42]:
 
 
 from doc_tool import DocTool
@@ -10,44 +10,44 @@ import matplotlib.pyplot as plt
 from node2vec import Node2Vec
 
 
-# In[16]:
+# In[43]:
 
 
 dt = DocTool()
 docContent = dt.readFile("dataset/englishText1.txt")
 
 
-# In[3]:
+# In[44]:
 
 
 pureSentences = dt.getPureSentences(docContent)
 
 
-# In[4]:
+# In[45]:
 
 
 print("Sentences:",pureSentences)
 
 
-# In[5]:
+# In[46]:
 
 
 words = dt.sentences2Words(pureSentences)
 
 
-# In[6]:
+# In[47]:
 
 
 print("Words:",words)
 
 
-# In[7]:
+# In[48]:
 
 
 textGraph = nx.Graph()
 
 
-# In[8]:
+# In[49]:
 
 
 # trace all text and extract simple graph. Each node/word connected with it's prev. word
@@ -57,50 +57,60 @@ for i in range(1,len(words)):
     textGraph.add_edge(words[i-1],words[i],weight=1)
 
 
-# In[11]:
+# In[54]:
 
 
 print("Nodes[",textGraph.number_of_nodes(),"]:",textGraph.nodes())
 
 
-# In[12]:
+# In[55]:
 
 
 print("Edges[",textGraph.number_of_edges(),"]:",textGraph.edges())
 
 
-# In[ ]:
+# In[76]:
 
 
-nx.draw_circular(textGraph,node_size=100,font_size=8,with_labels=True)
+nx.draw_circular(textGraph,width=0.5,node_size=80,font_size=8,with_labels=True)
 
 
-# In[ ]:
+# In[77]:
 
 
-plt.savefig("textGraph.pdf") # save graph to pdf file
+plt.savefig("outputs/textGraph.pdf") # save graph to pdf file
 
 
-# In[ ]:
+# In[78]:
 
 
 graphN2V = Node2Vec(textGraph, dimensions=64, walk_length=30, num_walks=200, workers=4) 
 
 
-# In[ ]:
+# In[79]:
 
 
 model = graphN2V.fit(window=10, min_count=1, batch_words=4)  # Any keywords acceptable by gensim.Word2Vec can be passed, `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
 
 
-# In[ ]:
+# In[80]:
 
 
-model.wv.most_similar("model")  # Output node names are always strings
+testWord = "document"
+similarities = model.wv.most_similar(testWord)  # Output node names are always strings
+
+#save and print results
+with open("outputs/similarity.txt","w") as f:
+    f.write("Word:"+testWord+"\n")
+    f.write(str(similarities))
+
+print("Similarities of",testWord)
+print(similarities)
+    
 
 
-# In[ ]:
+# In[81]:
 
 
-model.wv.save_word2vec_format("save.txt")
+model.wv.save_word2vec_format("outputs/node2vec_output.txt")
 
