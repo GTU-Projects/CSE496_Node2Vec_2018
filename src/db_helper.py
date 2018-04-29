1553d026fd1f9a5152bc65b791348479b331d0db
Hasan Men <hmen.56@gmail.com>
2018-04-29 23:03:01 +0300

    DB_Helper Updated. DB dependendices were decreased.

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,33 +1,36 @@
-import MySQLdb
 from datetime import datetime
+import json
+import copy
 
 class Tweet():
-    def __init__(self,sentName,text,date):
-        self.sentName = sentName
+    def __init__(self,username, userID, tweetID, text,date):
+        self.username = username
         self.text = text
         self.date = date
+        self.userID = userID
+        self.tweetID = tweetID
 
 class DBHelper:
 
     def __init__(self):
         pass
-    
 
-    def connect(self,user,passwd,db,host="localhost"):
+    def connectMysql(self,user,passwd,db,host="localhost"):
+        import MySQLdb
         self.db = MySQLdb.connect(host=host,user=user,passwd=passwd,db=db,charset="utf8",init_command="SET NAMES UTF8")
         self.cursor = self.db.cursor()
 
-    def getTweets(self,tableName):
-        self.cursor.execute(""" SELECT * FROM {tname} ORDER BY DateCreatedAt ASC;""".format(tname=tableName))
+    def getTweetsFromMysql(self,tableName):
+        self.cursor.execute(""" SELECT SentBy, UserID, ID, TextText, DateCreatedAt FROM {tname};""".format(tname=tableName))
         
         tweets = []
         for x in self.cursor.fetchall():
             # Input1: Wed Aug 10 01:34:24 EEST 2016 -> Wed Aug 10 01:34:24 EEST 2016 OUTPUT->
-            formattedTime = x[7].replace("EEST","").replace("EET","")
+            formattedTime = x[4].replace("EEST","").replace("EET","")
             formattedTime = datetime.strptime(formattedTime,'%a %b %d %H:%M:%S %Y')
-            tweets.append(Tweet(x[0],x[5],formattedTime))
+            tweets.append(Tweet(username=x[0],text=x[3],userID=x[1],tweetID=x[2],date=formattedTime))
         return tweets
-
+    
     def getUniqueUserIDs(self, tableName):
         self.cursor.execute(""" SELECT DISTINCT UserID FROM {tname}""".format(tname=tableName))
         userIDs = [user[0] for user in self.cursor.fetchall()]
@@ -38,11 +41,33 @@ class DBHelper:
         friendIDs = [friend[0] for friend in self.cursor.fetchall()]
         return friendIDs
 
+def saveTweetsAsJson(filePath, tweets):
+    
+    with open(filePath,'w+') as f:
+        tempList = []
+        for tweet in tweets:
+            temp = copy.copy(tweet)
+            temp.date = temp.date.strftime("%Y-%m-%d %H:%M:%S")
+            tempList.append(temp.__dict__)
+        json.dump(fp=f, obj=tempList)
+        
+def getTweetsFromJson(filePath):
+    
+    tweetList = []
+    with open(filePath,'r') as f:
+        jsonObjs = json.load(f)
+        
+        for obj in jsonObjs:
+            tweet = Tweet(**obj)
+            tweet.date = datetime.strptime(tweet.date,"%Y-%m-%d %H:%M:%S")
+            tweetList.append(tweet)
+    return tweetList
+
 if __name__=="__main__":
     dbHelper = DBHelper()
 
     # special config parameters wont work on your environment
-    dbHelper.connect(user="root",passwd="Hasan5695*",db="cse496")
+    dbHelper.connect(user="root",passwd="Hasan5695*",db="SocialMediaDB")
     tweets= dbHelper.getTweets(tableName="denemeShort")
     print("TweetSentName:",tweets[0].sentName,"TweetSentText:",tweets[0].text,"TweetSentDate:",tweets[0].date)
 
---
+++ src/utils.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+def draw_tsne(model):
+    "Creates and TSNE model and plots it"
+    labels = []
+    tokens = []
+
+    for word in model.wv.vocab:
+        tokens.append(model.wv[word])
+        labels.append(word)
+    
+    tsne_model = TSNE(perplexity=50, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+        
+    plt.figure(figsize=(25, 25)) 
+    for i in range(len(x)):
+        plt.scatter(x[i],y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
